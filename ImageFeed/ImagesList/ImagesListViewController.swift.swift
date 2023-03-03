ab5aff3e88e54c81cb9ec8acca8780b6075fba25
Alex Larionov <larry_xander@icloud.com>
2023-03-04 07:54:07 +0900

    Add UiTableViewDelegate & UiTableViewDataSource

--- ImageFeed/ImagesList/ImagesListViewController.swift.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
-class ImagesListViewController: UIViewController {
+class ImagesListViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
     @IBOutlet private var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
 }
 
