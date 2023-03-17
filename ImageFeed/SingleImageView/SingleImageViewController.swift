28e2e9461f7fcec51392de0bcebf4837d967a5cf
Alex Larionov <larry_xander@icloud.com>
2023-03-17 11:45:36 +0600

    Created SingleImageViewController

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by  Александр  on 17.03.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
