4faecd24f5b709bf1829802c3441320b6cbb2f72
Alex Larionov <larry_xander@icloud.com>
2023-03-18 18:00:51 +0600

    Added a button to go to the previous screen

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -22,5 +22,9 @@ final class SingleImageViewController: UIViewController {
           super.viewDidLoad()
           imageView.image = image
       }
+    
+    @IBAction private func didTapBackButton() {
+           dismiss(animated: true, completion: nil)
+       }
 }
 
